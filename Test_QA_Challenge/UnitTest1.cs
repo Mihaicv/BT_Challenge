0304965cb054b93d04646f51a25bc176067c2e92
Mihaicv <mihaivoicu1977@gmail.com>
2023-01-09 23:53:54 +0200

    Renamed list name

--- Test_QA_Challenge/UnitTest1.cs
+++ Test_QA_Challenge/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using QaChallenge;
 
@@ -12,38 +11,38 @@ namespace TestQaChallenge
         [Fact]
         public void TestFoundIndexSumIntEqualLeftWithRight()
         {
-            int[] arrayA = { 1, 2, 3, 4, 3, 2, 1 };
-            int resultIndex = indexSearch.Return_smallest_index(arrayA);
+            int[] arrayIntegers = { 1, 2, 3, 4, 3, 2, 1 };
+            int resultIndex = indexSearch.Return_smallest_index(arrayIntegers);
             Assert.Equal(3, resultIndex);
         }
 
         [Fact]
         public void TestEmptyArray()
         {
-            int[] arrayA = { };
-            int resultIndex = indexSearch.Return_smallest_index(arrayA);
+            int[] arrayIntegers = { };
+            int resultIndex = indexSearch.Return_smallest_index(arrayIntegers);
             Assert.Equal(0, resultIndex);
         }
 
         [Fact]
         public void TestIndexZeroEqualSumRestElements()
         {
-            int[] arrayA = { 20, 10, -80, 10, 10, 15, 35 };
-            int resultIndex = indexSearch.Return_smallest_index(arrayA);
+            int[] arrayIntegers = { 20, 10, -80, 10, 10, 15, 35 };
+            int resultIndex = indexSearch.Return_smallest_index(arrayIntegers);
             Assert.Equal(0, resultIndex);
         }
         [Fact]
         public void TestNoFoundedIndex()
         {
-            int[] arrayA = { 1, 2, 10, 4, 5, 18 };
-            int resultIndex = indexSearch.Return_smallest_index(arrayA);
+            int[] arrayIntegers = { 1, 2, 10, 4, 5, 18 };
+            int resultIndex = indexSearch.Return_smallest_index(arrayIntegers);
             Assert.Equal(-1, resultIndex);
         }
         [Fact]
         public void TestOnlyNegativeNumbers()
         {
-            int[] arrayA = { -1, -2, -3, -4, -3, -2, -1 };
-            int resultIndex = indexSearch.Return_smallest_index(arrayA);
+            int[] arrayIntegers = { -1, -2, -3, -4, -3, -2, -1 };
+            int resultIndex = indexSearch.Return_smallest_index(arrayIntegers);
             Assert.Equal(3, resultIndex);
         }
     }
